062f45e88e91949faa9e0f5cb73c9714f5c413f7
Gathrean Dela Cruz <gathrean@icloud.com>
2023-10-16 12:58:43 -0700

    Added comment for context, and renamed int lights to int n

--- Lab2.java
+++ Lab2.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList;
 
+/**
+ * The Grand Witch's request (demand).
+ * <p>
+ * She wants tp see coloured light strings displaying palindromes,
+ * and she wants to see all the possible palindromes.
+ */
 public class Lab2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
     
-    public ArrayList<String> generatePalindromeSequences(int lights) {
+    public ArrayList<String> generatePalindromeSequences(int n) {
         ArrayList<String> result = new ArrayList<String>();
 
         return result;
