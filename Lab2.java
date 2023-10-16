9a10e8e0aa925d7ed1afc9fef133433434577a53
Gathrean Dela Cruz <gathrean@icloud.com>
2023-10-16 13:06:31 -0700

    Added helper functions generatePalindromes and isPalindrome

--- Lab2.java
+++ Lab2.java
@@ -8,13 +8,36 @@ import java.util.ArrayList;
  */
 public class Lab2 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Lab2 lab = new Lab2();
+        int n = 3;
+        ArrayList<String> palindromes = lab.generatePalindromeSequences(n);
+        System.out.println(palindromes);
     }
-    
-    public ArrayList<String> generatePalindromeSequences(int n) {
-        ArrayList<String> result = new ArrayList<String>();
 
+    public ArrayList<String> generatePalindromeSequences(int n) {
+        ArrayList<String> result = new ArrayList<>();
+        generatePalindromes(n, "", result);
         return result;
     }
-}
 
+    private void generatePalindromes(int n, String current, ArrayList<String> result) {
+        if (current.length() == n) {
+            if (isPalindrome(current))
+                result.add(current);
+        } else {
+            for (char c = 'A'; c <= 'C'; c++) {
+                generatePalindromes(n, current + c, result);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String str) {
+        int len = str.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (str.charAt(i) != str.charAt(len - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
